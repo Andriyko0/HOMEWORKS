c4734bb75d6c0ea05d95fa07fffd0d1ec0f550e8
Andriyko0 <123240639+Andriyko0@users.noreply.github.com>
2023-01-28 12:52:24 +0200

    Add files via upload

---
+++ homework7.py
@@ -0,0 +1,85 @@
+import random
+#1
+#spusok1 = []
+#while len(spusok1)< 20:
+#    spusok1.append(random.randint(1,20))
+#print(spusok1)
+#spusok2 = []
+#while len(spusok2)< 20:
+#    spusok2.append(random.randint(1,20))
+#print(spusok2)
+#spilni=[]
+#for a in spusok1:
+#    for b in spusok2:
+#        if a == b :
+#            if a in spilni:
+#              tn = 1
+#            else:spilni.append(a)
+#print(spilni)
+#unikal=[]
+#for a in spusok1:
+#    for b in spusok2:
+#        if a == b:
+#            s=1
+#        elif a in spilni:
+#            d=1
+#        elif a in unikal:
+#            f=2
+#        else:
+#            unikal.append(a)
+#print(unikal)
+
+#2
+#spusok3 =[]
+#while len(spusok3) < 20:
+#    spusok3.append(random.randint(50,100))
+#print(spusok3)
+#while len(spusok3)!=0:
+#    print(spusok3[0])
+#    spusok3.pop(0)
+#spusok3 = []
+#while len(spusok3) < 20:
+#    spusok3.append(random.randint(50,100))
+#print(spusok3)
+#for i in spusok3:
+#    print(i)
+#3
+# numers = []
+#while len(numers) < 20:
+#    numers.append(random.randint(10,100))
+#print(numers)
+#newlist=[]
+#for x in numers:
+#  if len(newlist)<10:
+#    newlist.append(x)
+#
+#print(newlist)
+#4
+#l=['Hello']
+#while len(l)<10:
+#    l.append(random.randint(1,99))
+#print(l)
+#w = tuple(l)
+#print(w)
+#5
+#set1 =set()
+#while len(set1) < 10:
+#    set1.add(random.randint(1,10))
+#set2 =set()
+#while len(set2) < 10:
+#    set2.add(random.randint(1,15))
+#print(set1)
+#print(set2)
+#set3 = set1.intersection(set2)
+#print(set3)
+#set4=set1.symmetric_difference(set2)
+#print(set4)
+#pas =''
+#while len(pas)<8:
+#    pas += random.choice('ABCDEFGHIGKLMNOPQRSTUVYXWZ')
+#    pas += random.choice("1234567890")
+#    pas += random.choice('abcdefghigklmnopqrstuvyxwz')
+#    pas += random.choice('#$%&')
+#    if len(pas)==8:
+#        print(pas)
+
